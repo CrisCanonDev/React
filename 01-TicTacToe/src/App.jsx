32cc8377246bea8e22e63f6daa937c0011b00c57
CrisCanonDev <criscanon21@gmail.com>
2024-02-08 15:29:41 +0200

    Check winner  or tie via states . Implementing a resetGame useState

--- 01-TicTacToe/src/App.jsx
+++ 01-TicTacToe/src/App.jsx
@@ -8,12 +8,24 @@ const TURNS = {
   X: 'x',
   O: 'o'
 }
+const WINNER_POSITIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
+
+
 //Square component  
 const Square = ({ children, selected, updateBoard, index }) => {
 
   const className = `square ${selected ? 'is-selected' : ''}`
-  const handleClick = () =>{
-    updateBoard()
+  const handleClick = () => {
+    updateBoard(index)
   }
 
   return (
@@ -30,13 +42,52 @@ const board = Array(9).fill(null)
 
 function App() {
   const [turn, setTurn] = useState(TURNS.X)
-
   const [board, setBoard] = useState(Array(9).fill(null))
-  const updateBoard =() =>{
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn);
-    
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const winnerPosition of WINNER_POSITIONS) {
+      const [a, b, c] = winnerPosition
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[b] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
     }
+    return null //No winner 
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square != null)
+  }
+
+  const updateBoard = (index) => {
+
+    if (board[index] || winner) return //Not update position if is not void or winner
+
+    const newBoard = [...board] //copy of board. Props and state are inmutable (lead to state discprancies), not modifiable 
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X //Change turn
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+    else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
   return (
     <main className='board'>
       <section className='game'>
@@ -46,10 +97,10 @@ function App() {
               <Square
                 key={index}
                 index={index}
-                updateBoard={updateBoard}
-                >
+                updateBoard={updateBoard} //Function passed to execute it when is clicked, not when rendered
+              >
 
-              {board[index]}
+                {board[index]}
               </Square>
             )
 
@@ -64,6 +115,28 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner != null && (
+          <section className='winner'>
+            <div className='text'>
+              <h3>
+                {
+                  winner === false ? "Tie" : "Win"
+                }
+              </h3>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Start again</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
