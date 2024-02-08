c3524114f2ef61d8027e8ab1b8b32db941c9d456
CrisCanonDev <criscanon21@gmail.com>
2024-02-08 11:34:31 +0200

    Adding useState for swapping turns and showing current player via styling class, props to handle onclick

--- 01-TicTacToe/src/App.jsx
+++ 01-TicTacToe/src/App.jsx
@@ -3,43 +3,67 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
-
+//Constant of players' turn
 const TURNS = {
   X: 'x',
-  Y: 'y'
+  O: 'o'
 }
 //Square component  
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, selected, updateBoard, index }) => {
+
+  const className = `square ${selected ? 'is-selected' : ''}`
+  const handleClick = () =>{
+    updateBoard()
+  }
+
   return (
-    <div className='square'>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
-
+//Initializing every TicTacToe position empty
 const board = Array(9).fill(null)
 
+
+
+
 function App() {
+  const [turn, setTurn] = useState(TURNS.X)
 
-
-
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const updateBoard =() =>{
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn);
+    
+    }
   return (
     <main className='board'>
-      <h1>Tic tac toe</h1>
       <section className='game'>
         {
           board.map((_, index) => {
             return (
               <Square
                 key={index}
-                index={index}>
+                index={index}
+                updateBoard={updateBoard}
+                >
+
+              {board[index]}
               </Square>
             )
 
           })
         }
       </section>
-
+      <section className='turn'>
+        <Square selected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square selected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
     </main>
   )
 }
